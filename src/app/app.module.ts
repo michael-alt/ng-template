477f65356d4c20e8891d417d7272a62186721333
Michael Alt <lean@michael-alt.info>
2021-11-26 18:30:57 +0100

    Rebuild layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
